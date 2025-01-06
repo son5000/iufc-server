820363acdde907ed45ab5d699e2ca391b878d5ab
Hanje Son <sonhanje@kakao.com>
2025-01-07 00:25:46 +0900

    실제 배포 서버코드 반영

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ import userRouter from './src/routes/userRoute.js';
 import postRouter from './src/routes/postRoute.js';
 import adminRouter from './src/routes/adminRoute.js';
 import dotenv from 'dotenv';
+import * as crypto from 'crypto';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 3000;
+
+// 32바이트의 세션 고유 비밀 키 생성
+function generateSecretKey() {
+  return crypto.randomBytes(32).toString('hex');
+}
+const secretKey = generateSecretKey();
+
 
 // CORS 설정
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'https://icutd.netlify.app', 'http://localhost:3000'], // Netlify와 로컬 개발 환경 허용
+  origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
   methods: ['GET', 'POST', 'DELETE', 'PATCH'], // 허용할 HTTP 메서드
   credentials: true, // 쿠키 전달 허용
   allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 헤더 설정
@@ -23,7 +31,7 @@ app.use(express.json());  // 요청 본문을 JSON 형태로 파싱
 
 // Express 세션 설정
 app.use(session({
-  secret: 'secret-key', // 세션 암호화에 사용되는 비밀 키
+  secret: secretKey, // 세션 암호화에 사용되는 비밀 키
   resave: false,
   saveUninitialized: false,
   cookie: {  
@@ -33,29 +41,19 @@ app.use(session({
   }
 }));
 
-// HTTP 요청을 HTTPS로 리다이렉트하는 중간 미들웨어 추가
 app.use((req, res, next) => {
-  if (!req.secure) {
-    const secureUrl = `https://${req.headers.host}${req.url}`;
-    return res.redirect(301, secureUrl);
-  }
+  console.log('Request received:', req.method, req.url);
   next();
 });
 
-// 로깅 미들웨어 모든 요청 로깅
-app.use((req, res, next) => {
-  console.log(`[${req.method}] ${req.url}`); // 요청 메서드와 URL 로깅
-  next(); 
+app.get('/', (req, res) => {
+    res.send('Welcome to the son5000  Express server!');
 });
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Express server!');
-});
 
 app.use('/user', userRouter);
 app.use('/post', postRouter);
 app.use('/admin', adminRouter);
->>>>>>> 037538f1237e6d4dd91641a4fb9fda49220a6537
 
 // 서버 실행
 app.listen(PORT, () => {
